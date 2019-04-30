2d9d2ebc44800a6fe866ed2edb77fcd09253f0e2
tangelia <tangelia.atlanta@gmail.com>
2019-04-30 19:21:09 -0400

    Add Recipe Model

---
+++ models/Recipe.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema
+
+const RecipeSchema = new Schema({
+    Id: ObjectID(),
+    category: String,
+    title: String,
+    img: String,
+    description: String,
+    preptime: String,
+    ingredients:[{String}],
+    directions: String,
+    author: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    created:Date.now(),
+    reviews:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
+})
+
+Schema.plugin(passportLocalMongoose);
+
+/*
+Id: ObjectID()
+category: "string"
+Title:"string"
+img:"string"
+description: "string"
+ingredients: array of objects
+prep time:"string"
+directions:"string"
+created: Date.now()
+author:OjectID pointing to user-ID
+reviews: array of objects referencing Reviews
+*/
+module.exports = mongoose.model('Recipe', RecipeSchema)
