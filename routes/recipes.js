ec7120a8500a5912dbcfffd42b119f7611538ab5
tangelia <tangelia.atlanta@gmail.com>
2019-04-30 16:08:47 -0400

    All recipes routes working

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,20 +3,41 @@ let router = express.Router()
 
 //GET post index /recipes
 router.get('/',(req,res,next)=>{
-    res.send('/recipes');
+    res.send('INDEX /recipes');
     });
 
-router.get('/new',(req,res,next)=>{
-    res.send('/recipes/new');
-        });
 
 //GET index /recipes
 //GET new  /recipes/new
+router.get('/new',(req,res,next)=>{
+    res.send('NEW /recipes/new');
+     });
+
 //POST create /recipes
+router.post('/',(req,res,next)=>{
+    res.send('CREATE /recipes');
+     });
+
+
 //GET show /recipes/:id
+router.get('/:id',(req,res,next)=>{
+    res.send('SHOW /recipes/:id');
+     });
+
 //GET edit /recipes/:id/edit
+router.get('/:id/edit',(req,res,next)=>{
+    res.send('EDIT /recipes/:id/edit');
+     });
+
 //PUT update  /recipes/:id
+router.put('/:id',(req,res,next)=>{
+    res.send('UPDATE /recipes/:id');
+     });
+
 //DELETE delete /recipes/:id
+router.delete('/:id',(req,res,next)=>{
+    res.send('DELETE /recipes/:id');
+     });
 
 
 
