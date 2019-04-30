c1e8af1e3da925f1daf069d6619309bc3ad99abe
tangelia <tangelia.atlanta@gmail.com>
2019-04-30 18:58:04 -0400

    Add User Model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    email: String,
+    img: String,
+    recipes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Recipe'
+    }]
+})
+
+UserSchema.plugin(passportLocalMongoose);
+
+/*
+Name: "string"
+Username:"string"
+profile_img:"string"
+email: "string"
+password: Hash from Passport
+recipes: array of objects referencing Recipes
+*/
+module.exports = mongoose.model('User', UserSchema)
