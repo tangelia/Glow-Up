f87cc144857baf91185c03a0953e1bb9a020e656
tangelia <tangelia.atlanta@gmail.com>
2019-05-08 16:41:59 -0400

    Db connection

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 // Connect to database
 if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+    mongoose.connect(process.env.MONGODB_URI);
   }
   else {
-    mongoose.connect('mongodb://localhost/Glow-Up', { useNewUrlParser: true });
+    mongoose.connect('mongodb://localhost/Glow-Up');
   }
   mongoose.connection.on('error', (err) => {
     console.error('MongoDB connection error: ' + err);
