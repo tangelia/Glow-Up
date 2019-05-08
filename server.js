fb6103d8a8bd190d98de4eb60f5da887f068c32b
tangelia <tangelia.atlanta@gmail.com>
2019-05-08 15:18:50 -0400

    Favicon import

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const favicon = require('serve-favicon')
+const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
